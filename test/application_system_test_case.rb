a80e4971a507a381d24237984a084c145a106b92
shodayuki <yshoda1227@gmail.com>
2020-09-22 03:09:15 +0900

    Finish layout and routes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  test "full title helper" do
-    assert_equal full_title, FILL_IN
-    assert_equal full_title("Help"), FILL_IN
-  end
 end
