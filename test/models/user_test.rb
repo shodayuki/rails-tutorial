63766ad5b290926c05b2ec909855c3f5f455a1a5
shodayuki <yshoda1227@gmail.com>
2020-09-22 17:18:21 +0900

    rails db:migrate

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,7 +39,6 @@ class UserTest < ActiveSupport::TestCase
 
   test "emeil addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
