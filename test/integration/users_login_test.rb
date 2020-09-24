a867ab0d18caedbd3005c98624c74055e545a7c4
shodayuki <yshoda1227@gmail.com>
2020-09-24 22:15:11 +0900

    ログイン失敗時のフラッシュメッセージを表示

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "login width invalid information" do
+    get login_path
+    assert_template'sessions/new'
+    post login_path, params: { session: { email: "", password: "" }}
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
