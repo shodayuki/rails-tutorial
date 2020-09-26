3fb8ce721baa6b1162f1275365a65049e5b3d34f
shodayuki <yshoda1227@gmail.com>
2020-09-26 14:04:01 +0900

    ユーザー登録中にログインを表示

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login width invalid information" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid email/invalid password" do
     get login_path
     assert_template'sessions/new'
     post login_path, params: { session: { email: "", password: "" }}
