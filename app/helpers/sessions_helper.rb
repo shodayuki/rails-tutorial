03797477a7a97232f5ca86b50a99e769d6364fbe
shodayuki <yshoda1227@gmail.com>
2020-09-26 23:24:53 +0900

    フレンドリーフォワーディング実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -41,6 +41,17 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
   # 現在のユーザーをログアウトする
   def log_out
     forget(current_user)
