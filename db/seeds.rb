214ee95fb7050d7e8e8bbf0cf130c7f78b8dea7e
shodayuki <yshoda1227@gmail.com>
2020-09-27 11:14:42 +0900

    CRUD実装完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
