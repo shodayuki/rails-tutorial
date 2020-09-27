6e73c195192dbcdd31c194d7a08537dca15fcb67
shodayuki <yshoda1227@gmail.com>
2020-09-27 16:13:11 +0900

    アカウント有効化テスト修正

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,14 +20,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
     end
-    assert_equal 1,ActionMailer::Base.deliveries.size
+    assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
     assert_not user.activated?
     # 有効化していない状態でログインしてみる
     log_in_as(user)
     assert_not is_logged_in?
     # 有効化トークンが不正な場合
-    get edit_account_activation_path(user.activation_token, email: user.email)
+    get edit_account_activation_path("invalid token", email: user.email)
     assert_not is_logged_in?
     # トークンは正しいがメールアドレスが無効な場合
     get edit_account_activation_path(user.activation_token, email: 'wrong')
