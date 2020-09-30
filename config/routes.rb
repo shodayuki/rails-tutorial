7b790bd1f374c4a34c1700e9234623ee849f166e
shodayuki <yshoda1227@gmail.com>
2020-10-01 07:52:48 +0900

    delete機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/microposts/:id', to: 'microposts#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
