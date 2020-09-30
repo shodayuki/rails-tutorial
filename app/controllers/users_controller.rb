a49fbb7422f128c15df41d22df7c4fa35b55ebf5
shodayuki <yshoda1227@gmail.com>
2020-09-30 20:53:30 +0900

    micropostフォーム作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,19 +54,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
     end
 
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
