22792b1f5d776f538b82faad90f0d9ee146cb7c7
shodayuki <yshoda1227@gmail.com>
2020-10-03 15:14:35 +0900

    フォローを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
-  has_many :active_relationship, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -72,6 +73,21 @@ class User < ApplicationRecord
   # 完全な実装は次章の「ユーザーをフォローする」を参照
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォロしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
